2ad581c47a2fcaeda5a8d6576f5438f67e1762b1
Mauricio3000 <mauriciosantoshoyos@gmail.com>
2020-01-20 10:23:16 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const port = 8080;
+const express = require("express")
+const cors = require("cors")
+
+//--- Express setup
+const app = express();
+app.use(cors())
+app.use(express.static('client'))
+
+app.listen({ port: port }, () => { })
